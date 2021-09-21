62416ff93596ea7d5f9dc282c2840f2c7de04dc7
Aziz Sydikov <aziz.sydikov@red-gate.com>
2021-09-21 21:16:34 +0100

    Try to fix 1

--- tests/TCSandBox.UnitTests/UnitTest1.cs
+++ tests/TCSandBox.UnitTests/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace TCSandBox.UnitTests
         [Test]
         public void Test1()
         {
-            Assert.Fail("This test is failed purposefully");
+            Assert.Fail("First attempt to fix this");
         }
     }
 }
