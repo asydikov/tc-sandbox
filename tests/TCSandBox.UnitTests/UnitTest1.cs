4a5aa977d2a1650fb67fb35502b8cf544e9e07b2
Aziz Sydikov <aziz.sydikov@red-gate.com>
2021-09-22 17:17:55 +0100

    Add unused code

--- tests/TCSandBox.UnitTests/UnitTest1.cs
+++ tests/TCSandBox.UnitTests/UnitTest1.cs
@@ -4,11 +4,6 @@ namespace TCSandBox.UnitTests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Test1()
         {
