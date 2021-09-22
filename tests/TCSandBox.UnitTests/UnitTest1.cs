333d82f0a9433f31d009f38f811b0e97ff6586fc
Aziz Sydikov <aziz.sydikov@red-gate.com>
2021-09-22 17:03:52 +0100

    Fail test

--- tests/TCSandBox.UnitTests/UnitTest1.cs
+++ tests/TCSandBox.UnitTests/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace TCSandBox.UnitTests
         [Test]
         public void Test1()
         {
-            Assert.Pass("Passed purposefully");
+            Assert.Fail("Failed  purposefully");
         }
     }
 }
